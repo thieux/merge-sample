b9345706f8bc59930667f2b5e9ba41d92d52a90c
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-11-24 22:20:49 +0100

    Add a constant to the formula

--- src/main/java/com/mathieupauly/mergesample/Extract.java
+++ src/main/java/com/mathieupauly/mergesample/Extract.java
@@ -2,7 +2,7 @@ package com.mathieupauly.mergesample;
 
 public class Extract {
     public void justDoIt(int n) {
-        int value = n * 2;
+        int value = (n * 2) + 1;
         System.out.println(value);
     }
 }
